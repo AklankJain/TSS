a0c00de9b63defb88a13cd69672bace6b67a2ab3
AklankJain <aj.aklank@live.com>
2016-05-10 20:08:59 +0530

    last page

--- CMS/CMS/Program.cs
+++ CMS/CMS/Program.cs
@@ -17,7 +17,7 @@ namespace CMS
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            Application.Run(new Page2UI());
+            Application.Run(new Page8UI());
 
         }
     }
