98f5e29dba4a479e6421dec414873366cea54df5
AklankJain <aj.aklank@live.com>
2016-05-10 15:43:47 +0530

    i sometimes get confused about why we need to commit this time

--- CMS/CMS/Page2UI.Designer.cs
+++ CMS/CMS/Page2UI.Designer.cs
@@ -38,33 +38,37 @@
             // textBox1
             // 
             this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.Right;
-            this.textBox1.Location = new System.Drawing.Point(315, 280);
+            this.textBox1.Location = new System.Drawing.Point(284, 358);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(160, 20);
+            this.textBox1.Size = new System.Drawing.Size(212, 22);
             this.textBox1.TabIndex = 0;
             // 
             // textBox2
             // 
             this.textBox2.Anchor = System.Windows.Forms.AnchorStyles.Right;
-            this.textBox2.Location = new System.Drawing.Point(702, 280);
+            this.textBox2.Location = new System.Drawing.Point(936, 345);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(190, 20);
+            this.textBox2.Size = new System.Drawing.Size(252, 22);
             this.textBox2.TabIndex = 1;
             // 
             // textBox3
             // 
             this.textBox3.Anchor = System.Windows.Forms.AnchorStyles.Right;
-            this.textBox3.Location = new System.Drawing.Point(704, 350);
+            this.textBox3.Location = new System.Drawing.Point(939, 431);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(190, 20);
+            this.textBox3.Size = new System.Drawing.Size(252, 22);
             this.textBox3.TabIndex = 2;
             // 
             // textBox4
             // 
             this.textBox4.Anchor = System.Windows.Forms.AnchorStyles.Right;
-            this.textBox4.Location = new System.Drawing.Point(788, 419);
+            this.textBox4.Location = new System.Drawing.Point(1051, 516);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(168, 20);
+            this.textBox4.Size = new System.Drawing.Size(223, 22);
             this.textBox4.TabIndex = 3;
             // 
             // comboBox1
@@ -76,25 +80,27 @@
             "4th and below",
             "5th and 6th",
             "7th and above"});
-            this.comboBox1.Location = new System.Drawing.Point(315, 350);
+            this.comboBox1.Location = new System.Drawing.Point(284, 442);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(160, 21);
+            this.comboBox1.Size = new System.Drawing.Size(212, 24);
             this.comboBox1.TabIndex = 4;
             // 
             // Page2UI
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.BackgroundImage = global::CMS.Properties.Resources.without_icon_02;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1008, 729);
+            this.ClientSize = new System.Drawing.Size(1344, 897);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Page2UI";
             this.Text = "Details";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
