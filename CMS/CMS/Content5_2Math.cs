1bc7dae0be3812394c333c46ca6b12618255bf2a
AklankJain <aj.aklank@live.com>
2016-05-11 14:50:47 +0530

    changes in the content folder , made the path relative

---
+++ CMS/CMS/Content5_2Math.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CMS
+{
+    public partial class Content5_2Math : Form
+    {
+        public Content5_2Math()
+        {
+            InitializeComponent();
+            FormBorderStyle = FormBorderStyle.None;
+            WindowState = FormWindowState.Maximized;
+            axShockwaveFlash1.Movie = Application.StartupPath + @"\..\..\56\Math\2\main.swf";
+            axShockwaveFlash1.Play();
+        }
+    }
+}
