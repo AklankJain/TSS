e0c71f4a7beeacb39d4845bf32187aa9cbd3df74
parthapk05 <parthakurmi@gmail.com>
2016-05-11 19:43:42 -0700

    Minor changes

--- CMS/CMS/Page1UI.Designer.cs
+++ CMS/CMS/Page1UI.Designer.cs
@@ -28,44 +28,47 @@
        /// </summary>
        private void InitializeComponent()
        {
-           System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Page1UI));
-           this.button1 = new System.Windows.Forms.Button();
-           this.SuspendLayout();
-           // 
-           // button1
-           // 
-           this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-           this.button1.AutoSize = true;
-           this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
-           this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-           this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
-           this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-           this.button1.Location = new System.Drawing.Point(820, 600);
-           this.button1.Margin = new System.Windows.Forms.Padding(0);
-           this.button1.Name = "button1";
-           this.button1.Size = new System.Drawing.Size(149, 109);
-           this.button1.TabIndex = 0;
-           this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
-           this.button1.UseVisualStyleBackColor = true;
-           this.button1.Click += new System.EventHandler(this.button1_Click);
-           // 
-           // Page1UI
-           // 
-           this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-           this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-           this.AutoSize = true;
-           this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-           this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-           this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-           this.ClientSize = new System.Drawing.Size(1008, 729);
-           this.Controls.Add(this.button1);
-           this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-           this.Name = "Page1UI";
-           this.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultBounds;
-           this.Text = "Welcome";
-           this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
-           this.ResumeLayout(false);
-           this.PerformLayout();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Page1UI));
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.AutoSize = true;
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(232)))), ((int)(((byte)(233)))));
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(900, 630);
+            this.button1.Margin = new System.Windows.Forms.Padding(0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 80);
+            this.button1.TabIndex = 0;
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Page1UI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1008, 729);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.Color.Transparent;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Page1UI";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultBounds;
+            this.Text = "Welcome";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
        }
 
